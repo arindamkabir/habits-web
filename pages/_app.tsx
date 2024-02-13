ecc2d171dd7fdb850fced3477acaa1fbd1c3f011
Arindam Kabir <arindamkabir@gmail.com>
2024-02-14 02:27:22 +0600

    feat: add poppins and react-query provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,40 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+import { Poppins } from 'next/font/google';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
 import { ThemeProvider } from "@/components/providers/theme-provider";
 
-export default function App({ Component, pageProps }: AppProps) {
+const poppins = Poppins({ subsets: ['latin'], weight: ['200', '400', '500', '600', '700'], variable: '--font-poppins' });
+
+const CustomApp = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: { queries: { staleTime: 40000 } }
+  }));
+
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${poppins.style.fontFamily};
+        }
+      `}</style>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className={`${poppins.className} min-h-screen`} data-theme="dark">
+            <Component {...pageProps} />
+          </main>
+        </ThemeProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider >
+    </>
+  )
 }
+
+export default CustomApp;
