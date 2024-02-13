8a0500a8ffc66b94d137bf1618cf99a3aee57f20
Arindam Kabir <arindamkabir@gmail.com>
2024-02-14 01:55:02 +0600

    feat: project initialization

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <div className="h-screen w-screen flex justify-center align-center">
+      <Card>
+        asdfadf
+      </Card>
+    </div>
+  );
+}
