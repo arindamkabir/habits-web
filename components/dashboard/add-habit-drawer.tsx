ac6b05055f56e3b01554cf3855a5988432bc23a4
Arindam Kabir <arindamkabir@gmail.com>
2024-02-15 01:06:54 +0600

    feat: add new habit drawer

---
+++ components/dashboard/add-habit-drawer.tsx
@@ -0,0 +1,21 @@
+import FormDrawer from '@/components/ui/form-drawer'
+import useBoundedStore from '@/store/store';
+import React from 'react'
+import AddHabitForm from './forms/add-habit-form';
+
+const AddHabitDrawer = () => {
+    const addHabitDrawerOpen = useBoundedStore(state => state.addHabitDrawerOpen);
+    const openAddHabitDrawer = useBoundedStore(state => state.openAddHabitDrawer);
+
+    return (
+        <FormDrawer
+            title={"Add New Habit"}
+            open={addHabitDrawerOpen}
+            onClose={() => openAddHabitDrawer(false)}
+        >
+            <AddHabitForm />
+        </FormDrawer>
+    )
+}
+
+export default AddHabitDrawer
