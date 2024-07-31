8ab2b966d3c1733050933cbc238def7014770989
Arindam Kabir <arindamkabir@gmail.com>
2024-08-01 02:38:53 +0600

    feat: add habit card

---
+++ components/features/habits/habit-card.tsx
@@ -0,0 +1,54 @@
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { bgClasses, textClasses } from "@/config/colors";
+import { HabitWithEntries } from "@/types/Habit";
+import { cn } from "@/utils/classNames"
+import { formatDate } from "date-fns"
+import { useMemo } from "react";
+
+type HabitCardProps = {
+    className?: string;
+    habit: HabitWithEntries;
+    dates: Date[];
+}
+
+export function HabitCard({ className, habit, dates }: HabitCardProps) {
+    const entries = useMemo(() => dates.map((date) => ({
+        date,
+        habitEntry: habit.entries.find((entry) => formatDate(new Date(entry.date), "yyyy-MM-dd") === formatDate(date, "yyyy-MM-dd"))
+    })), [dates, habit.entries]);
+
+    const bgClass = bgClasses[habit.category.color as keyof typeof bgClasses];
+    const textClass = textClasses[habit.category.color as keyof typeof textClasses];
+
+    return (
+        <Card className={cn("w-[380px]", className)}>
+            <CardHeader>
+                <CardTitle>{habit.name}</CardTitle>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+                <div className="flex justify-between">
+                    {
+                        entries.map((item) => (
+                            <div key={`${habit.id}-${item.date.toString()}`} className="flex flex-col items-center space-y-1">
+                                <div className="text-xs">
+                                    {formatDate(item.date, "EEE")}
+                                </div>
+                                <div className={cn(
+                                    "flex justify-center items-center rounded-full h-8 w-8 text-xs",
+                                    item.habitEntry ? (item.habitEntry.entry ? `${bgClass} text-white` : `bg-slate-900 ${textClass}`) : "bg-slate-600 text-slate-400"
+                                )}>
+                                    {formatDate(item.date, "d")}
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </CardContent>
+        </Card>
+    )
+}
