c60d7bdf0dae53ee80f259cf6b530a3dca03f934
Arindam Kabir <arindamkabir@gmail.com>
2024-08-04 23:53:17 +0600

    feat: add habit entry drawer

--- components/features/habits/modals/save-entry-modal.tsx
+++ components/features/habits/modals/save-entry-modal.tsx
@@ -6,21 +6,41 @@ import {
 } from "@/components/ui/dialog";
 import useAppStore from "@/store/store";
 import SaveEntryForm from "../forms/save-entry-form";
+import { useMediaQuery } from 'usehooks-ts'
+import { Drawer, DrawerContent, DrawerHeader, DrawerTitle } from "@/components/ui/drawer";
 
 const SaveEntryModal = () => {
+    const isDesktop = useMediaQuery("(min-width: 768px)")
+
     const saveEntryModalOpen = useAppStore(state => state.saveEntryModalOpen);
     const openSaveEntryModal = useAppStore(state => state.openSaveEntryModal);
 
-    return (
-        <Dialog open={saveEntryModalOpen} onOpenChange={openSaveEntryModal}>
-            <DialogContent>
-                <DialogHeader>
-                    <DialogTitle>Add new entry</DialogTitle>
-                </DialogHeader>
+    if (isDesktop) {
+        return (
+            <Dialog open={saveEntryModalOpen} onOpenChange={openSaveEntryModal}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Add new entry</DialogTitle>
+                    </DialogHeader>
 
-                <SaveEntryForm />
-            </DialogContent>
-        </Dialog>
+                    <SaveEntryForm />
+                </DialogContent>
+            </Dialog>
+        )
+    }
+
+    return (
+        <Drawer open={saveEntryModalOpen} onOpenChange={openSaveEntryModal}>
+            <DrawerContent>
+                <DrawerHeader className="text-left">
+                    <DrawerTitle>Add new entry</DrawerTitle>
+                </DrawerHeader>
+
+                <div className="px-4 pb-6">
+                    <SaveEntryForm />
+                </div>
+            </DrawerContent>
+        </Drawer>
     )
 }
 
--- pages/habits/[slug].tsx
+++ pages/habits/[slug].tsx
@@ -1,15 +1,12 @@
 import DashboardLayout from '@/components/layouts/DashboardLayout';
-import { habitDetailsPrefetchQuery, useGetHabitDetails } from '@/hooks/queries/use-get-habit-details'
+import { habitDetailsPrefetchQuery } from '@/hooks/queries/use-get-habit-details'
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { useRouter } from 'next/router'
 import { getCookie, setCookie } from 'cookies-next';
 import { DehydratedState, HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { cn } from '@/utils/classNames';
-import { formatDate } from 'date-fns';
-import { useMemo } from 'react';
-import { bgClasses, textClasses } from '@/config/colors';
-import { generateDatesByMonth, weekDayNames } from '@/utils/dates';
+import { HabitCalendar } from '@/components/features/habits/calendar/habit-calendar';
+import SaveEntryModal from '@/components/features/habits/modals/save-entry-modal';
+import { HabitMonthlyChart } from '@/components/features/habits/chart/habit-monthly-chart';
 
 export const getServerSideProps = (async (context) => {
     const queryClient = new QueryClient();
@@ -43,76 +40,16 @@ const HabitDetailsPage = ({
 ) => {
     const router = useRouter();
 
-    const { data: habitDetails } = useGetHabitDetails({ slug: router.query.slug as string });
-
-    const dates = useMemo(() => {
-        return generateDatesByMonth(Date.now());
-    }, []);
-
-    const entries = useMemo(() => dates.map((week) => {
-        return week.map((date) => ({
-            date,
-            habitEntry: habitDetails?.data.entries
-                .find((entry) =>
-                    formatDate(new Date(entry.date), "yyyy-MM-dd") === formatDate(date, "yyyy-MM-dd")
-                )
-        }));
-    }), [dates, habitDetails?.data.entries]);
-
-    console.log(entries);
-
-    const bgClass = bgClasses[habitDetails?.data.category.color as keyof typeof bgClasses];
-    const textClass = textClasses[habitDetails?.data.category.color as keyof typeof textClasses];
-
     return (
         <DashboardLayout>
             <HydrationBoundary state={dehydratedState}>
-                <Card
-                    className={cn("w-full md:w-[380px]")}
-                >
-                    <CardHeader>
-                        <CardTitle>{habitDetails?.data.name}</CardTitle>
-                    </CardHeader>
-                    <CardContent className="grid gap-4">
-                        <div className="flex justify-between pb-2 border-b border-zinc-800">
-                            {
-                                weekDayNames.map((day) => (
-                                    <div key={day} className="text-xs w-8 text-center">
-                                        {day}
-                                    </div>
-                                ))
-                            }
-                        </div>
-                        <div className="space-y-2">
-                            {
-                                entries.map((week, index) => (
-                                    <div key={index} className="flex justify-between">
-                                        {
-                                            week.map((entry) => {
-                                                return (
-                                                    <div key={entry.toString()}
-                                                        className={cn(
-                                                            "flex justify-center items-center rounded-lg h-8 w-8 text-[11.5px] font-semibold",
-                                                            entry.habitEntry
-                                                                ? (
-                                                                    entry.habitEntry.entry
-                                                                        ? `${bgClass} text-white`
-                                                                        : `bg-zinc-900 ${textClass}`
-                                                                )
-                                                                : "bg-zinc-600 text-zinc-400"
-                                                        )}
-                                                    >
-                                                        {formatDate(entry.date, "d")}
-                                                    </div>
-                                                )
-                                            })
-                                        }
-                                    </div>
-                                ))
-                            }
-                        </div>
-                    </CardContent>
-                </Card>
+                <div className="space-y-2">
+                    <HabitCalendar
+                        slug={router.query.slug as string}
+                    />
+                    <HabitMonthlyChart />
+                </div>
+                <SaveEntryModal />
             </HydrationBoundary>
         </DashboardLayout>
     )
